35be57e1bff38e917b3e8836336e21a934bbc5f0
C1321759 Jose Marcos Vinicius Andrade Gois <c1321759@interno.bb.com.br>
2023-12-17 16:20:06 -0300

    first commit

---
+++ Server.py
@@ -0,0 +1,45 @@
+import argparse
+import RDT
+import time
+
+
+def upperCase(message):
+    capitalizedSentence = message.upper()
+    return capitalizedSentence
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='UPPER CASE server.')
+    parser.add_argument('port', help='Port.', type=int)
+    args = parser.parse_args()
+
+    timeout = 5000  # close connection if no new data within 5 seconds
+    time_of_last_data = time.time()
+
+    rdt = RDT.RDT('server', None, args.port)
+    try:
+        while True:
+            # try to receiver message before timeout
+            msg_S = rdt.rdt_3_0_receive()
+            if msg_S is None:
+                if time_of_last_data + timeout < time.time():
+                    break
+                else:
+                    continue
+            time_of_last_data = time.time()
+
+            # convert and reply
+            rep_msg_S = upperCase(msg_S)
+            print('Server: converted %s \nto\n %s' % (msg_S, rep_msg_S))
+            rdt.rdt_3_0_send(rep_msg_S)
+    except (KeyboardInterrupt, SystemExit):
+        rdt.disconnect()
+        print("Ending connection...")
+    except (BrokenPipeError, ConnectionAbortedError, ConnectionResetError):
+        rdt.disconnect()
+        print("Ending connection...")
+    finally:
+        rdt.disconnect()
+        print("Connection ended.")
+
+
